b6b729517288859a26b6cb5b7c7ff9c310198e51
Aneurin Barker Snook <a@aneur.in>
2021-08-30 18:14:04 +0100

    chore: remove typedocs, favour descriptive var names

--- src/device/cli.ts
+++ src/device/cli.ts
@@ -1,25 +1,18 @@
 import { Command } from 'commander'
 
-const registerAction = (p: Command) => () => {
-  console.debug('device register WIP', p.opts())
+const registerAction = (parent: Command) => () => {
+  console.debug('device register WIP', parent.opts())
 }
 
-/**
- * Create a device CLI.
- *
- * Includes subcommand to register a device.
- *
- * @param p Parent program
- */
-export const withProgram = (p: Command): void => {
+export const withProgram = (parent: Command): void => {
   const deviceCLI = new Command('device')
 
   // edge device register
   const register = new Command('register')
     .description('register this device on the network')
-    .action(registerAction(p))
+    .action(registerAction(parent))
 
   deviceCLI.addCommand(register)
 
-  p.addCommand(deviceCLI)
+  parent.addCommand(deviceCLI)
 }
--- src/stake/cli.ts
+++ src/stake/cli.ts
@@ -1,40 +1,33 @@
 import { Command } from 'commander'
 
-const createAction = (p: Command) => () => {
-  console.debug('stake create WIP', p.opts())
+const createAction = (parent: Command) => () => {
+  console.debug('stake create WIP', parent.opts())
 }
 
-const listAction = (p: Command) => () => {
-  console.debug('stake ls WIP', p.opts())
+const listAction = (parent: Command) => () => {
+  console.debug('stake ls WIP', parent.opts())
 }
 
-const releaseAction = (p: Command, rc: Command) => (id: string) => {
-  console.debug('stake release WIP', p.opts(), rc.opts(), id)
+const releaseAction = (parent: Command, release: Command) => (id: string) => {
+  console.debug('stake release WIP', parent.opts(), release.opts(), id)
 }
 
-const unlockAction = (p: Command) => (id: string) => {
-  console.debug('stake unlock WIP', p.opts(), id)
+const unlockAction = (parent: Command) => (id: string) => {
+  console.debug('stake unlock WIP', parent.opts(), id)
 }
 
-/**
- * Create a staking CLI.
- *
- * Includes subcommands to create, release, and unlock stakes.
- *
- * @param p Parent program
- */
-export const withProgram = (p: Command): void => {
+export const withProgram = (parent: Command): void => {
   const stakeCLI = new Command('stake')
 
   // edge stake create
   const create = new Command('create')
     .description('assign a new stake to this device')
-    .action(createAction(p))
+    .action(createAction(parent))
 
   // edge stake ls
   const list = new Command('ls')
     .description('list all stakes')
-    .action(listAction(p))
+    .action(listAction(parent))
 
   // edge stake release
   const release = new Command('release')
@@ -46,13 +39,13 @@ export const withProgram = (p: Command): void => {
       'The --express option instructs the blockchain to take a portion of your stake in return for an immediate ',
       'release of funds, rather than waiting for the unlock period to conclude.'
     ].join(''))
-  release.action(releaseAction(p, release))
+  release.action(releaseAction(parent, release))
 
   // edge stake unlock
   const unlock = new Command('unlock')
     .argument('<id>', 'stake ID')
     .description('unlock a stake')
-    .action(unlockAction(p))
+    .action(unlockAction(parent))
 
   stakeCLI
     .addCommand(create)
@@ -60,5 +53,5 @@ export const withProgram = (p: Command): void => {
     .addCommand(release)
     .addCommand(unlock)
 
-  p.addCommand(stakeCLI)
+  parent.addCommand(stakeCLI)
 }
