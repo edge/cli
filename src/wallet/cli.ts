8df6cc097cb61106b5f36c963412a66f962f1ec0
Aneurin Barker Snook <a@aneur.in>
2021-10-12 12:11:07 +0100

    feat: improve wallet info, restore ux

--- src/wallet/cli.ts
+++ src/wallet/cli.ts
@@ -1,7 +1,7 @@
 import * as xe from '@edge/xe-utils'
 import { Command, Option } from 'commander'
 import { ask, askSecure } from '../input'
-import { defaultFile, readWallet, withFile } from './storage'
+import { decryptFileWallet, defaultFile, readWallet, withFile } from './storage'
 import { errorHandler, getOptions as getGlobalOptions } from '../edge/cli'
 import { readFileSync, writeFileSync } from 'fs'
 
@@ -9,7 +9,7 @@ const createAction = (parent: Command, createCmd: Command) => async () => {
   const opts = {
     ...getGlobalOptions(parent),
     ...getWalletOption(parent),
-    ...await (async () => {
+    ...(() => {
       const {
         privateKeyFile,
         overwrite
@@ -19,23 +19,21 @@ const createAction = (parent: Command, createCmd: Command) => async () => {
       }>()
       return { privateKeyFile, overwrite }
     })(),
-    ...await getPassphraseOption(createCmd)
+    ...getPassphraseOption(createCmd)
   }
 
   const { check, write } = withFile(opts.wallet)
 
-  if (await check()) {
-    let confirm = opts.overwrite ? 'y' : ''
-    if (confirm.length === 0) {
-      const ynRegexp = /^[yn]$/
-      while (confirm.length === 0) {
-        const input = await ask('A wallet already exists. Overwrite? [yn] ')
-        if (ynRegexp.test(input)) confirm = input
-        else console.log('Please enter y or n.')
-      }
-      if (confirm === 'n') return
-      console.log()
+  if (await check() && !opts.overwrite) {
+    let confirm = ''
+    const ynRegexp = /^[yn]$/
+    while (confirm.length === 0) {
+      const input = await ask('A wallet already exists. Overwrite? [yn] ')
+      if (ynRegexp.test(input)) confirm = input
+      else console.log('Please enter y or n.')
     }
+    if (confirm === 'n') return
+    console.log()
   }
 
   if (!opts.passphrase) {
@@ -104,54 +102,96 @@ const createHelp = [
   'This should be copied to a secure location and kept secret.'
 ].join('')
 
-const infoAction = (parent: Command) => async () => {
+const infoAction = (parent: Command, infoCmd: Command) => async () => {
   const opts = {
     ...getGlobalOptions(parent),
-    ...getWalletOption(parent)
+    ...getWalletOption(parent),
+    ...getPassphraseOption(infoCmd)
   }
 
   const wallet = await readWallet(opts.wallet)
-
-  console.log('wallet address:', wallet.address)
-  console.log('wallet file:', opts.wallet)
+  console.log(`Wallet address: ${wallet.address}`)
+  if (opts.passphrase) {
+    try {
+      const decrypted = decryptFileWallet(wallet, opts.passphrase)
+      console.log(`Private key: ${decrypted.privateKey}`)
+    }
+    catch (err) {
+      console.log(`Cannot display private key: ${(err as Error).message}`)
+    }
+  }
 }
+
+const infoHelp = [
+  '\n',
+  'This command displays information about your wallet.\n\n',
+  'If a passphrase is provided, this command will also decrypt and display your private key.'
+].join('')
 
 const restoreAction = (parent: Command, restoreCmd: Command) => async () => {
   const opts = {
     ...getGlobalOptions(parent),
     ...getWalletOption(parent),
-    ...await getPrivateKeyOption(restoreCmd),
-    ...await getPassphraseOption(restoreCmd)
+    ...(() => {
+      const { overwrite } = restoreCmd.opts<{ overwrite: boolean }>()
+      return { overwrite }
+    })(),
+    ...getPrivateKeyOption(restoreCmd),
+    ...getPassphraseOption(restoreCmd)
   }
 
-  const { write } = withFile(opts.wallet)
+  const { check, write } = withFile(opts.wallet)
+
+  if (await check() && !opts.overwrite) {
+    let confirm = ''
+    const ynRegexp = /^[yn]$/
+    while (confirm.length === 0) {
+      const input = await ask('A wallet already exists. Overwrite? [yn] ')
+      if (ynRegexp.test(input)) confirm = input
+      else console.log('Please enter y or n.')
+    }
+    if (confirm === 'n') return
+    console.log()
+  }
+
+  if (!opts.privateKey) {
+    const privateKey = await askSecure('Please enter a private key: ')
+    if (privateKey.length === 0) throw new Error('private key required')
+    opts.privateKey = privateKey
+    console.log()
+  }
+
+  if (!opts.passphrase) {
+    console.log('To ensure your wallet is secure it will be encrypted locally using a passphrase.')
+    console.log('For more information, see https://wiki.edge.network/TODO')
+    console.log()
+    const passphrase = await askSecure('Please enter a passphrase: ')
+    if (passphrase.length === 0) throw new Error('passphrase required')
+    const confirmKey = await askSecure('Please confirm passphrase: ')
+    if (confirmKey !== passphrase) throw new Error('passphrases do not match')
+    opts.passphrase = passphrase
+    console.log()
+  }
 
   const wallet = xe.wallet.recover(opts.privateKey || '')
-  await write(wallet, opts.passphrase || '')
-
-  console.log('wallet address:', wallet.address)
-  console.log('wallet file:', opts.wallet)
+  await write(wallet, opts.passphrase)
+  console.log(`Wallet ${wallet.address} restored.`)
 }
 
-export const addPrivateKeyOption = (cmd: Command): void =>
-  [privateKeyOption(), privateKeyFileOption()].forEach(opt => cmd.addOption(opt))
+const restoreHelp = [
+  '\n',
+  'This command will restore an existing wallet using a private key you already have.\n\n',
+  'You will be asked to provide a passphrase to encrypt the wallet locally. ',
+  'The passphrase is also required later to decrypt the wallet for certain actions, such as signing transactions.\n\n'
+].join('')
 
 export const addPassphraseOption = (cmd: Command): void =>
   [passphraseOption(), passphraseFileOption()].forEach(opt => cmd.addOption(opt))
 
-export const getPrivateKeyOption = async (cmd: Command): Promise<{ privateKey?: string }> => {
-  const { privateKey, privateKeyFile: file } = cmd.opts<Record<'privateKey' | 'privateKeyFile', string|undefined>>()
-  if (privateKey && privateKey.length) return { privateKey }
-  // read secure value from file if set
-  if (file !== undefined) {
-    if (file.length === 0) throw new Error('no path to passphrase file')
-    const data = readFileSync(file)
-    return { privateKey: data.toString() }
-  }
-  return {}
-}
+export const addPrivateKeyOption = (cmd: Command): void =>
+  [privateKeyOption(), privateKeyFileOption()].forEach(opt => cmd.addOption(opt))
 
-export const getPassphraseOption = async (cmd: Command): Promise<{ passphrase?: string }> => {
+export const getPassphraseOption = (cmd: Command): { passphrase?: string } => {
   const { passphrase, passphraseFile: file } = cmd.opts<Record<'passphrase' | 'passphraseFile', string|undefined>>()
   if (passphrase && passphrase.length) return { passphrase }
   // read secure value from file if set
@@ -159,6 +199,18 @@ export const getPassphraseOption = async (cmd: Command): Promise<{ passphrase?: 
     if (file.length === 0) throw new Error('no path to passphrase file')
     const data = readFileSync(file)
     return { passphrase: data.toString() }
+  }
+  return {}
+}
+
+export const getPrivateKeyOption = (cmd: Command): { privateKey?: string } => {
+  const { privateKey, privateKeyFile: file } = cmd.opts<Record<'privateKey' | 'privateKeyFile', string|undefined>>()
+  if (privateKey && privateKey.length) return { privateKey }
+  // read secure value from file if set
+  if (file !== undefined) {
+    if (file.length === 0) throw new Error('no path to passphrase file')
+    const data = readFileSync(file)
+    return { privateKey: data.toString() }
   }
   return {}
 }
@@ -188,12 +240,17 @@ export const withProgram = (parent: Command): void => {
   addPassphraseOption(create)
   create.action(errorHandler(parent, createAction(parent, create)))
 
-  const info = new Command('info').description('display wallet info')
+  const info = new Command('info')
+    .description('display wallet info')
+    .addHelpText('after', infoHelp)
   addPassphraseOption(info)
-  info.action(errorHandler(parent, infoAction(parent)))
+  info.action(errorHandler(parent, infoAction(parent, info)))
 
   // edge wallet restore
-  const restore = new Command('restore').description('restore a wallet')
+  const restore = new Command('restore')
+    .description('restore a wallet')
+    .addHelpText('after', restoreHelp)
+    .option('-f, --overwrite', 'overwrite existing wallet if one exists')
   addPrivateKeyOption(restore)
   addPassphraseOption(restore)
   restore.action(errorHandler(parent, restoreAction(parent, restore)))
