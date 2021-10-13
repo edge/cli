f0d45ecd38faa054e66a11d4853f7e8ee066673b
Aneurin Barker Snook <a@aneur.in>
2021-10-13 10:40:25 +0100

    chore: add licensing

--- src/edge/cli.ts
+++ src/edge/cli.ts
@@ -1,3 +1,7 @@
+// Copyright (C) 2021 Edge Network Technologies Limited
+// Use of this source code is governed by a GNU GPL-style license
+// that can be found in the LICENSE.md file. All rights reserved.
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Command } from 'commander'
 import { Network, selectNetwork } from '../config'
