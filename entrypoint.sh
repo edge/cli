e22220f8b61295996cb5b58654ef8bb8c69e2983
Adam K Dean <adamkdean@googlemail.com>
2021-11-09 21:26:04 +0000

    feat: implement entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,45 @@
-#!/bin/bash
+#!/bin/sh
 # Copyright (C) 2021 Edge Network Technologies Limited
 # Use of this source code is governed by a GNU GPL-style license
 # that can be found in the LICENSE.md file. All rights reserved.
 
-env
+set -e
 
-# mkdir -p /mnt/fileserver/cli/$NETWORK/linux/x64/$VERSION && \
-# mkdir -p /mnt/fileserver/cli/$NETWORK/linux/arm64/$VERSION && \
-# mkdir -p /mnt/fileserver/cli/$NETWORK/macos/x64/$VERSION && \
-# mkdir -p /mnt/fileserver/cli/$NETWORK/macos/arm64/$VERSION && \
-# mkdir -p /mnt/fileserver/cli/$NETWORK/windows/x64/$VERSION && \
-# mkdir -p /mnt/fileserver/cli/$NETWORK/windows/arm64/$VERSION && \
-# cp /cli/bin/edge-linux-x64 /mnt/fileserver/cli/$NETWORK/linux/x64/$VERSION/$FILENAME
+# $1 is platform, $2 is arch, $3 is version, $4 is filename
+copyFile() {
+  mkdir -p /mnt/fileserver/cli/$NETWORK/$1/$2/$3
+  cp /cli/bin/edge-$1-$2 /mnt/fileserver/cli/$NETWORK/$1/$2/$3/$4
+  chmod +x /mnt/fileserver/cli/$NETWORK/$1/$2/$3/$4
+  echo $3 > /mnt/fileserver/cli/$NETWORK/$1/$2/$3/version
+  sha256sum /mnt/fileserver/cli/$NETWORK/$1/$2/$3/$4 > /mnt/fileserver/cli/$NETWORK/$1/$2/$3/checksum
+}
+
+main() {
+  declare -a PLATFORMS=("linux" "macos" "windows")
+  declare -a ARCHS=("x64" "arm64")
+
+  # If network or version are not set, exit
+  if [ -z $NETWORK || -z $VERSION ]; then
+    echo "Usage: NETWORK=<network> VERSION=<version> ./entrypoint.sh"
+    exit 1
+  fi
+
+  # Set filename to edge if network is mainnet otherwise set it to edgetest
+  if [ $NETWORK ]; then
+    FILENAME="edge"
+  else
+    FILENAME="edgetest"
+  fi
+
+  # Loop through platforms and then archs, copying files
+  for platform in "${PLATFORMS[@]}"
+  do
+    for arch in "${ARCHS[@]}"
+    do
+      copyFile $platform $arch $VERSION $FILENAME
+      copyFile $platform $arch latest $FILENAME
+    done
+  done
+}
+
+main "$@"; exit
