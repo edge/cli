517be74707b488193f41a75773440a4efc40f560
Adam K Dean <adamkdean@googlemail.com>
2021-11-11 14:56:34 +0000

    feat: remove arch from file matrix in entrypoint, use env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,10 @@ set -e
 
 main() {
   declare -a PLATFORMS=("linux" "macos" "windows")
-  declare -a ARCHS=("x64" "arm64")
 
   # If network or version are not set, exit
-  if [[ -z $NETWORK || -z $VERSION ]]; then
-    echo "Usage: NETWORK=<network> VERSION=<version> ./entrypoint.sh"
+  if [[ -z $ARCH || -z $NETWORK || -z $VERSION ]]; then
+    echo "Usage: ARCH=<arch> NETWORK=<network> VERSION=<version> ./entrypoint.sh"
     exit 1
   fi
 
@@ -22,35 +21,27 @@ main() {
     FILENAME="edgetest"
   fi
 
-  # Loop through platforms and then archs, copying files,
-  # except for windows/arm64, which we skip
+  # Loop through platforms copying files
   for platform in "${PLATFORMS[@]}"
   do
-    for arch in "${ARCHS[@]}"
-    do
-      DEST="${FILENAME}"
-      SRC="edge-${platform}-${arch}"
+    DEST="${FILENAME}"
+    SRC="edge-${platform}"
 
-      # Append .exe to the filename if windows and skip
-      # windows/arm64 until we are able to support it
-      if [ $platform = "windows" ]; then
-        if [ $arch = "arm64" ]; then
-          continue
-        fi
-        DEST="${DEST}.exe"
-        SRC="${SRC}.exe"
-      fi
+    # Append .exe to the filename if windows
+    if [ $platform = "windows" ]; then
+      DEST="${DEST}.exe"
+      SRC="${SRC}.exe"
+    fi
 
-      # Here we ensure the version directory exists, then copy the binary into it renaming it to the correct name,
-      # then update the version file with the correct version, generate a checksum and update the checksum file,
-      # before finally removing and updating the latest directory to point to the correct version
-      mkdir -p /mnt/fileserver/cli/$NETWORK/$platform/$arch/$VERSION
-      cp /cli/bin/$SRC /mnt/fileserver/cli/$NETWORK/$platform/$arch/$VERSION/$DEST
-      echo $VERSION > /mnt/fileserver/cli/$NETWORK/$platform/$arch/$VERSION/version
-      sha256sum /mnt/fileserver/cli/$NETWORK/$platform/$arch/$VERSION/$DEST | head -c 64 > /mnt/fileserver/cli/$NETWORK/$platform/$arch/$VERSION/checksum
-      rm -rf /mnt/fileserver/cli/$NETWORK/$platform/$arch/latest
-      cp -r /mnt/fileserver/cli/$NETWORK/$platform/$arch/$VERSION /mnt/fileserver/cli/$NETWORK/$platform/$arch/latest
-    done
+    # Here we ensure the version directory exists, then copy the binary into it renaming it to the correct name,
+    # then update the version file with the correct version, generate a checksum and update the checksum file,
+    # before finally removing and updating the latest directory to point to the correct version
+    mkdir -p /mnt/fileserver/cli/$NETWORK/$platform/$ARCH/$VERSION
+    cp /cli/bin/$SRC /mnt/fileserver/cli/$NETWORK/$platform/$ARCH/$VERSION/$DEST
+    echo $VERSION > /mnt/fileserver/cli/$NETWORK/$platform/$ARCH/$VERSION/version
+    sha256sum /mnt/fileserver/cli/$NETWORK/$platform/$ARCH/$VERSION/$DEST | head -c 64 > /mnt/fileserver/cli/$NETWORK/$platform/$ARCH/$VERSION/checksum
+    rm -rf /mnt/fileserver/cli/$NETWORK/$platform/$ARCH/latest
+    cp -r /mnt/fileserver/cli/$NETWORK/$platform/$ARCH/$VERSION /mnt/fileserver/cli/$NETWORK/$platform/$ARCH/latest
   done
 }
 
