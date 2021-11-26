588168c899c8f8c0765d0a25685a46a6fdfb37ce
Aneurin Barker Snook <a@aneur.in>
2021-11-26 14:58:03 +0000

    fix: invalid hash directive

--- .build/build.sh
+++ .build/build.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 # Copyright (C) 2021 Edge Network Technologies Limited
 # Use of this source code is governed by a GNU GPL-style license
 # that can be found in the LICENSE.md file. All rights reserved.
