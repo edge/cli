136d9964166dc99e1858f94fc2c592b92809b661
Aneurin Barker Snook <a@aneur.in>
2021-11-26 14:57:48 +0000

    chore: add missing license comments

--- .build/auto_target.sh
+++ .build/auto_target.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Copyright (C) 2021 Edge Network Technologies Limited
+# Use of this source code is governed by a GNU GPL-style license
+# that can be found in the LICENSE.md file. All rights reserved.
+
 set -e
 
 if [ "${BUILD_TARGET}" != "" ]; then
--- src/device/data.ts
+++ src/device/data.ts
@@ -1,3 +1,7 @@
+// Copyright (C) 2021 Edge Network Technologies Limited
+// Use of this source code is governed by a GNU GPL-style license
+// that can be found in the LICENSE.md file. All rights reserved.
+
 import * as xe from '@edge/xe-utils'
 import config from '../config'
 import { normalizedPlatform } from '../helpers'
