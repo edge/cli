a041b350f65c5d83b9fd9f391a4ebacec1e21688
Aneurin Barker Snook <a@aneur.in>
2021-12-10 10:34:46 +0000

    chore: update some community links

--- src/about/cli.ts
+++ src/about/cli.ts
@@ -4,16 +4,16 @@ const communityAction = () => async () => {
   console.log([
     'Browse public Edge code on GitHub:\n',
     '  https://github.com/edge\n\n',
-    'Chat with the community on Telegram, Discord, or Reddit:\n',
+    'Chat with the community on Discord, Telegram, or Reddit:\n',
+    '  https://discord.gg/edge-network\n',
     '  https://t.me/edgenetwork\n',
-    '  https://discord.gg/3sEvuYJ\n',
     '  https://reddit.com/r/edgenetwork\n\n',
     'Follow Edge on Twitter or Facebook:\n',
     '  https://twitter.com/edgenetwork\n',
     '  https://www.facebook.com/edgenetworktech\n\n',
     'Keep up with Edge on Medium or sign up to the mailing list:\n',
-    '  https://medium.com/dadi\n',
-    '  https://edge.network/en/'
+    '  https://edge.medium.com\n',
+    '  https://edge.press'
   ].join(''))
 }
 
