ab3732677322e5aadd0aea39675e56f14c889ae8
Aneurin Barker Snook <a@aneur.in>
2022-12-22 08:04:50 +0000

    chore: update copyright year in new/changed files

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-// Copyright (C) 2021 Edge Network Technologies Limited
+// Copyright (C) 2022 Edge Network Technologies Limited
 // Use of this source code is governed by a GNU GPL-style license
 // that can be found in the LICENSE.md file. All rights reserved.
 
